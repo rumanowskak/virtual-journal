8e454c1b501c9ba1ad027d56a1c8c76f5974d222
Katarzyna Rumanowska <kasia.rumanowska@gmail.com>
2024-02-25 17:19:44 +0100

    Init repository

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import StartPage from './components/StartPage/StartPage';
+
+function App() {
+  return (
+    <div className="App">
+      <StartPage/>
+    </div>
+  );
+}
+
+export default App;
