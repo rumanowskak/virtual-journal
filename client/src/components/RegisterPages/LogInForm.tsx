3eea15df5ba8eb55b7eae3f4e42aeb948545e577
Katarzyna Rumanowska <kasia.rumanowska@gmail.com>
2024-03-02 20:57:25 +0100

    Init register process

--- client/src/components/RegisterPages/LogInForm.tsx
+++ client/src/components/RegisterPages/LogInForm.tsx
@@ -1,17 +1,58 @@
 import '../../css/RegisterPages/LogInForm.css'
 
+import { useState } from "react";
+import { useSetRecoilState } from 'recoil';
+
+import { UserState } from '../../atoms/User';
+import { useNavigate } from 'react-router';
+
+
 function LogInForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const setUser = useSetRecoilState(UserState);
+  const navigate = useNavigate();
+
+
+  function handleSubmit(e: any) {
+    e.preventDefault();
+    fetch( '/api/users/login',
+    {
+      method:"POST",
+      headers: {
+        Accept: '*',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password,
+    })
+    }
+  )
+    .then((res) => {
+      return res.json()
+    })
+    .then((data)=>{
+      console.log(data);
+      setUser(data);
+      navigate('/home');
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  }
     return (
       <div className="log-in-form">
-        <form >
+        <form onSubmit={(e) => handleSubmit(e)}>
             <label className='label-email'>
                 email:
-                <input type="text" name="email" placeholder='email'></input>
+                <input type="text" name="email" placeholder='email' value={email} onChange={(e)=>{setEmail(e.target.value)}}></input>
             </label>
             <label className='label-password'>
                 password:
-                <input type="password" name="password" placeholder='password'></input>
+                <input type="password" name="password" placeholder='password' value={password} onChange={(e)=>{setPassword(e.target.value)}}></input>
             </label>
+            <button>Submit</button>
         </form>
       </div>
     );
--- client/src/components/RegisterPages/SignUpForm.tsx
+++ client/src/components/RegisterPages/SignUpForm.tsx
@@ -1,21 +1,86 @@
 import '../../css/RegisterPages/SignUpForm.css'
+import { useState } from "react";
+import { useNavigate } from 'react-router';
+import { useSetRecoilState } from 'recoil';
+
+import { UserState } from '../../atoms/User';
 
 function SignUpForm() {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const navigate = useNavigate();
+  const setUser = useSetRecoilState(UserState);
+
+  function handleSubmit(e: any) {
+    e.preventDefault();
+
+    fetch('/api/users/register',
+      {
+        method:"POST",
+        headers: {
+          Accept: '*',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password,
+          username: username
+      })
+      }
+    )
+    .then(res => {
+        loginAfterSignUp();
+        navigate('/home');
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+function loginAfterSignUp() {
+  fetch( '/api/users/login',
+    {
+      method:"POST",
+      headers: {
+        Accept: '*',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password,
+    })
+    }
+  )
+    .then((res) => {
+      return res.json()
+    })
+    .then((data)=>{
+      setUser(data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+
     return (
       <div className="sign-up-form">
-        <form >
+        <form onSubmit={(e) => handleSubmit(e)} >
             <label className='label-email'>
                 email:
-                <input type="text" name="email" placeholder='email'></input>
+                <input type="text" name="email" placeholder='email' value={email} onChange={(e)=>{setEmail(e.target.value)}}></input>
             </label>
             <label className='label-password'>
                 password:
-                <input type="password" name="password" placeholder='password'></input>
+                <input type="password" name="password" placeholder='password' value={password} onChange={(e)=>{setPassword(e.target.value)}}></input>
             </label>
             <label className='label-username'>
                 username:
-                <input type="text" name="username" placeholder='username'></input>
+                <input type="text" name="username" placeholder='username' value={username} onChange={(e)=>{setUsername(e.target.value)}}></input>
             </label>
+            <button>Submit</button>
         </form>
       </div>
     );
